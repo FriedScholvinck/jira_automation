05b6a92cfd9548a81040dc3edb65ceada8f704ba
fried <f.scholvinck@amsterdam.nl>
2024-01-16 08:59:33 +0100

    other test

--- app/test.py
+++ app/test.py
@@ -1,6 +1,11 @@
-# create test app for azure webapp
 import streamlit as st
 
 st.title("Hello Azure Webapp")
 st.write("This is a test app for azure webapp")
 
+# import sys
+# from streamlit import cli as stcli
+
+# if __name__ == '__main__':
+#     sys.argv = ["streamlit", "run", "APP_NAME.py"]
+#     sys.exit(stcli.main())
